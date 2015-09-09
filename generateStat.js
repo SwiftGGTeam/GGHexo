080342f5a490d3645e6ca895be99d24b51b8d7a0
梁杰 <lj925184928@gmail.com>
2015-09-09 17:15:41 +0800

    update stat scripts

--- generateStat.js
+++ generateStat.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 
-let basePath = './source/_posts'
+let basePath = './src'
 let pageHeader = `
 # SwiftGG团队贡献榜
 
@@ -36,6 +36,11 @@ let finalHeader = `
 `
 let targetFile = './source/stat/index.md'
 
+function* entries(obj) {
+   for (let key of Object.keys(obj)) {
+     yield [key, obj[key]];
+   }
+}
 
 let originInfo = new Promise(function (resolve, reject) {
   fs.readdir(basePath, (err, files) => {
@@ -64,21 +69,37 @@ let originInfo = new Promise(function (resolve, reject) {
 //   }
 //   }
 // ))
-.then(contents => contents.map(
-  content => ({
+.then(contents => contents.map(content => {
+  let regs = {
+    translators: '译者=(.*)',
+    auditors: '校对=(.*)',
+    finalmans: '定稿=(.*)'
+  }
+  let info = {}
+  for (let [key, value] of entries(regs)) {
+    let reg = new RegExp(value)
+    info[key] = content.match(reg)[1].trim()
+  }
+  return {
     words: content.substr(content.indexOf("---") + 3).replace(/\s+/g, "").length,
-    translator: content.match(/> 译者：(\[.*?\]\(.*?)[\/]?\)/)[1] + ")",
-    auditor: content.match(/> 校对：(\[.*?\]\(.*?)[\/]?\)/)[1] + ")",
-    finalMan: content.match(/> 定稿：(\[.*?\]\(.*?)[\/]?\)/)[1] + ")",
-  })
-))
+    translators: info.translators.split(","),
+    auditors: info.auditors.split(","),
+    finalMans: info.finalmans.split(","),
+  }
+}))
+
 
 let finalStat = originInfo
 .then(contentArr => contentArr.reduce(
   (contentMap, item) => 
-    contentMap.has(item.finalMan) ? 
-      contentMap.set(item.finalMan, contentMap.get(item.finalMan) + 1) : 
-      contentMap.set(item.finalMan, 1),
+  {
+    for (let finalMan of item.finalMans) {
+      contentMap.has(finalMan) ? 
+      contentMap.set(finalMan, contentMap.get(finalMan) + 1) : 
+      contentMap.set(finalMan, 1)
+    }
+    return contentMap
+  },
   new Map()
 ))
 .then(contentMap => Array.from(contentMap).sort((a, b) => b[1] - a[1]))
@@ -91,9 +112,14 @@ let finalStat = originInfo
 let auditStat = originInfo
 .then(contentArr => contentArr.reduce(
   (contentMap, item) => 
-    contentMap.has(item.auditor) ? 
-      contentMap.set(item.auditor, contentMap.get(item.auditor) + 1) : 
-      contentMap.set(item.auditor, 1),
+  {
+    for (let auditor of item.auditors) {
+      contentMap.has(auditor) ? 
+      contentMap.set(auditor, contentMap.get(auditor) + 1) : 
+      contentMap.set(auditor, 1)
+    }
+    return contentMap
+  },
   new Map()
 ))
 .then(contentMap => Array.from(contentMap).sort((a, b) => b[1] - a[1]))
@@ -105,10 +131,14 @@ let auditStat = originInfo
 
 let translationInfo = originInfo
 .then(contentArr => contentArr.reduce(
-  (contentMap, item) => 
-    contentMap.has(item.translator) ? 
-      contentMap.set(item.translator, [contentMap.get(item.translator)[0] + item.words, contentMap.get(item.translator)[1] + 1]) : 
-      contentMap.set(item.translator, [item.words, 1]),
+  (contentMap, item) => {
+    for (let translator of item.translators) {
+      contentMap.has(translator) ? 
+      contentMap.set(translator, [contentMap.get(translator)[0] + item.words, contentMap.get(translator)[1] + 1]) : 
+      contentMap.set(translator, [item.words, 1])
+    }
+    return contentMap
+  },
   new Map()
 ))
 
