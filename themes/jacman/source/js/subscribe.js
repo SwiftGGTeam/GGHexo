31f8e71099f6e7dd8719376d62b9d575c512fea6
梁杰 <lj925184928@gmail.com>
2015-09-10 18:17:37 +0800

    add function

---
+++ themes/jacman/source/js/subscribe.js
@@ -0,0 +1,39 @@
+$(function() {
+  var checker = localStorage.getItem("swiftweeklysubscribed")
+  if (checker) {
+    return
+  } else {
+    function checkemail(val) {
+      if (!val.trim()) {
+        return false
+      }
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+      return re.test(val)
+    }
+    $(document).scroll(function() {
+      var y = $(this).scrollTop()
+      if (y > 400) {
+        $('#swiftweekly').fadeIn()
+      }
+    })
+    $(".sw_sub").click(function() {
+      $("#swiftweekly #content").submit()
+    })
+    $("#swiftweekly #content").submit(function(e) {
+      e.preventDefault()
+      if (!checkemail($("#sw_email").val())) {
+        setTimeout('$("#sw_email").focus()', 100)
+        return
+      }
+      $("#sw_email").addClass("disabled")
+      $(".sw_sub").addClass("disabled").text("稍等")
+      $.getJSON("http://ggchecker.githuber.info/addemail/" + $("#swiftweekly #sw_email").val().trim(), function(data) {
+        if (data["success"] == "success") {
+          localStorage.setItem("swiftweeklysubscribed", true)
+          $(".sw_sub").text("成功！")
+          setTimeout("$('#swiftweekly').fadeOut()", 1000);
+        }
+      })
+    })
+  }
+})
