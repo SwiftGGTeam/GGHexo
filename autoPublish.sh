dc0be7a7545d9b3171f8f4b683daedf0a421de40
numbbbbb <lj925184928@gmail.com>
2015-09-14 11:32:21 +0800

    update autoPublish script

--- autoPublish.sh
+++ autoPublish.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-cd /GGHexo
-git fetch origin
-reslog=$(git log HEAD..origin/master --oneline)
-if [[ "${reslog}" != "" ]] ; then
-  git merge origin/master
-  cd source
-  git pull
-  cd .. 
-  babel-node generatePosts.js
+cd /GGHexo/source
+git pull
+cd ..
+git pull
+file_num=$(ls /GGHexo/source/_posts -1 --file-type | grep -v '/$' | wc -l)
+babel-node generatePosts.js
+file_num_now=$(ls /GGHexo/source/_posts -1 --file-type | grep -v '/$' | wc -l)
+if (( file_num_now > file_num )); then
   babel-node generateStat.js
   babel-node generateShareMD.js
   python 3-extractImgs.py
@@ -22,4 +21,8 @@ if [[ "${reslog}" != "" ]] ; then
   git add .
   git commit -m 'AUTO: publish'
   git push
+else
+  git reset --hard HEAD
+  cd source
+  git reset --hard HEAD
  fi
