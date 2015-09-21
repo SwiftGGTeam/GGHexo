9e310c56faa7da4e7968c0e6053689546e9a450c
梁杰 <lj925184928@gmail.com>
2015-09-21 10:12:38 +0800

    update autoPublish

--- autoPublish.sh
+++ autoPublish.sh
@@ -7,6 +7,29 @@ REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @ @{u})
 if [ $LOCAL = $REMOTE ]; then
   echo "Up-to-date"
+  file_num=$(ls /GGHexo/source/_posts -1 --file-type | grep -v '/$' | wc -l)
+  babel-node generatePosts.js
+  file_num_now=$(ls /GGHexo/source/_posts -1 --file-type | grep -v '/$' | wc -l)
+  if (( file_num_now > file_num )); then
+    python 3-extractImgs.py
+    babel-node generateStat.js
+    babel-node generateShareMD.js
+    hexo clean
+    hexo g
+    hexo d
+    cd source
+    git add .
+    git commit -m 'AUTO: publish'
+    git push
+    cd ..
+    git add .
+    git commit -m 'AUTO: publish'
+    git push
+  else
+    git reset --hard HEAD
+    cd source
+    git reset --hard HEAD
+  fi
 elif [ $LOCAL = $BASE ]; then
   cd /GGHexo/source
   git pull
