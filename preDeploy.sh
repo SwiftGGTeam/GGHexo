beeb94f529ee15de7709c683d8251543a72b4ab5
Forelax <helloworldmkv@163.com>
2018-04-22 00:56:12 +0800

    split-master-and-stage-env

--- preDeploy.sh
+++ preDeploy.sh
@@ -9,19 +9,22 @@ if [[ $1 == 'local' ]]; then
   yarn run hexo s
 fi
 yarn run hexo clean
-if [[ $TRAVIS_BRANCH == "stage" ]]; then 
-  echo 'deploy to SwiftGG/SwiftGGTeam.github.io'
+if [[ $TRAVIS_BRANCH == "master" ]]; then 
+  echo 'deploy to SwiftGG/SwiftGGTeam.github.io aliyun-pages branch'
   cd source
   git checkout master
   git pull
-  echo 'stage.swift.gg' > ./CNAME
+  echo 'swift.gg' > ./CNAME
   git add .
   git commit -m 'AUTO: Publish'
   git push
   cd ..
-  git checkout stage
+  git checkout master
   git add .
   git commit -m 'AUTO: Publish'
-  git push 
+  git push
+elif [[ $TRAVIS_BRANCH == "stage" ]]; then
+  echo 'deploy to SwiftGG/SwiftGGTeam.github.io master branch'
+  echo 'stage.swift.gg' > source/CNAME
 fi
 yarn run hexo g --bail --silent
