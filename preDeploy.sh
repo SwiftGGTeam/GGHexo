88096c1a0c453a56401f0b8fb2e86d1b70d41a9b
Forelax <helloworldmkv@163.com>
2018-04-22 01:23:41 +0800

    Revert "split-master-and-stage-env"
    This reverts commit 5b508bc7f8150a7bc332b2215b1a26bffa6dd8b5.

--- preDeploy.sh
+++ preDeploy.sh
@@ -9,22 +9,19 @@ if [[ $1 == 'local' ]]; then
   yarn run hexo s
 fi
 yarn run hexo clean
-if [[ $TRAVIS_BRANCH == "master" ]]; then 
-  echo 'deploy to SwiftGG/SwiftGGTeam.github.io aliyun-pages branch'
+if [[ $TRAVIS_BRANCH == "stage" ]]; then 
+  echo 'deploy to SwiftGG/SwiftGGTeam.github.io'
   cd source
   git checkout master
   git pull
-  echo 'swift.gg' > ./CNAME
+  echo 'stage.swift.gg' > ./CNAME
   git add .
   git commit -m 'AUTO: Publish'
   git push
   cd ..
-  git checkout master
+  git checkout stage
   git add .
   git commit -m 'AUTO: Publish'
-  git push
-elif [[ $TRAVIS_BRANCH == "stage" ]]; then
-  echo 'deploy to SwiftGG/SwiftGGTeam.github.io master branch'
-  echo 'stage.swift.gg' > source/CNAME
+  git push 
 fi
 yarn run hexo g --bail --silent
