efe48f6d0a23b56e56e5e19065d5894155fb457b
Forelax <helloworldmkv@163.com>
2018-05-19 17:59:52 +0800

    fix merge pr problem

--- deployMaster.js
+++ deployMaster.js
@@ -18,20 +18,23 @@ axios.post("https://api.github.com/repos/SwiftGGTeam/SwiftGGTeam.github.io/pulls
     console.error(`${number} error`)
     process.exit(1)
   }
-  axios.put(`https://api.github.com/repos/SwiftGGTeam/SwiftGGTeam.github.io/pulls/${number}/merge`, {
-    merge_method: 'merge'
-  }, {
-    headers: {
-      'Authorization': `token ${CI_USER_TOKEN}`,
-    }
-  })
-  .then((result) => {
-    console.log('merge success! API result is:')
-    console.log(result.data)
-  }).catch((err) => {
-    console.log(err.response)
-    process.exit(1)
-  });
+  setTimeout(() => {
+    // sometimes if request merge after created, API return error. so we delay 3s
+    axios.put(`https://api.github.com/repos/SwiftGGTeam/SwiftGGTeam.github.io/pulls/${number}/merge`, {
+      merge_method: 'merge'
+    }, {
+        headers: {
+          'Authorization': `token ${CI_USER_TOKEN}`,
+        }
+      })
+      .then((result) => {
+        console.log('merge success! API result is:')
+        console.log(result.data)
+      }).catch((err) => {
+        console.log(err.response)
+        process.exit(1)
+      });
+  }, 3000 * 1000);
 }).catch((err) => {
   console.log(err.response.data.errors)
   process.exit(1)
