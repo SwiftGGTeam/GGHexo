0b010133af5404ac377e7df3d6af18feb29285fe
Jianing Wang <wjnmailg@gmail.com>
2019-07-10 19:55:24 +0800

    Add Ji4n1ng in nameMap

--- nameMap.js
+++ nameMap.js
@@ -75,7 +75,8 @@ let nameMap = {
   "石榴": "https://github.com/alejx",
   "RocZhang": "https://www.roczhang.com/",
   "张弛": "https://github.com/zhangchi25806",
-  "RickeyBoy": "https://github.com/RickeyBoy"
+  "RickeyBoy": "https://github.com/RickeyBoy",
+  "Ji4n1ng": "https://github.com/Ji4n1ng"
 }
 
 export default nameMap
